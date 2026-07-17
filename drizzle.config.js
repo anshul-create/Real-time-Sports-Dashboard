d4455b1b9cfbb788d17d01501de53d154a8b8b0e
anshul-create <as9572298@gmail.com>
2026-07-17 23:11:19 +0530

    Dependency added for Drizzle ORM

---
+++ drizzle.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "drizzle-kit";
+import "dotenv/config";
+
+
+if(!process.env.DATABASE_URL){
+    throw new Error('DATABASE_URI is not set in the .env file');
+}
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./src/schema.js",
+  out: "./drizzle",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+});
