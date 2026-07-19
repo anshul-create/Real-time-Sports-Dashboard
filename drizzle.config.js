42371773e36fc5947d2ba097208572580b67241a
anshul-create <as9572298@gmail.com>
2026-07-19 22:57:17 +0530

    Inplement database schemas

--- drizzle.config.js
+++ drizzle.config.js
@@ -8,7 +8,7 @@ if(!process.env.DATABASE_URL){
 
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./src/schema.js",
+  schema: "./src/db/schema.js",
   out: "./drizzle",
   dbCredentials: {
     url: process.env.DATABASE_URL,
